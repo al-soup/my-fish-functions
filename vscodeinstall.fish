f35f25c09c69afb8718b2c870d61401f47e3118c
al-soup <alex@kraeuchi.io>
2019-11-27 14:00:46 +0100

    Add ToDo; Fix typo

--- vscodeinstall.fish
+++ vscodeinstall.fish
@@ -1,10 +1,10 @@
-# TODO: Improve error handling
+# TODO: Improve error handling; Validate input file
 function vscodeinstall --a 'file' --description 'Install VS Code extensions from a file'
     if test -n "$file"
         while read -la line
             eval code --install-extension $line
         end < $file
     else
-        echo "Please provide a install .txt installation file"
+        echo "Please provide an installation file."
     end
 end
